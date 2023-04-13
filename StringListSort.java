b939f50c451fc8372ad7632b7022c6b9a584f6e3
Charles Sharp <25charlessharp@usd266.com>
2023-04-13 12:16:06 -0500

    almost done

--- StringListSort.java
+++ StringListSort.java
@@ -1,9 +1,38 @@
 import java.util.ArrayList;
 
-public class StringListSearch {
+public class StringListSort {
 
-    public static int unorderedSearch(ArrayList<String> items, String term) {
-        return 0;
+    public static void selectionSort(ArrayList<String> unordered) {
+        for(int i = 0; i < unordered.size(); i ++){
+            int index = i;
+            String smallestString = unordered.get(i);
+            for(int k = i + 1; k < unordered.size(); k ++){
+                if(unordered.get(i).compareTo(unordered.get(k)) > 0){
+                    index = k;
+                    smallestString = unordered.get(k);
+                }
+            }
+            String holder = unordered.get(i);
+            unordered.set(i, smallestString);
+            unordered.set(index, holder);
+            
+            
+        }
+        
+    }
+
+    public static void insertionSort(ArrayList<String> unordered){
+        for(int i = 1; i < unordered.size(); i ++){
+            int index = i - 1;
+            int k = i;
+            while(k >= 0 && unordered.get(i).compareTo(unordered.get(index)) < 0){
+                String holder = unordered.get(i);
+                unordered.set(i, unordered.get(index));
+                unordered.set(index, holder);
+                k --;
+            }
+
+        }
     }
     
     public static void main(String[] args) {
--- StringListSortTest.java
+++ StringListSortTest.java
@@ -8,6 +8,23 @@ import org.junit.Test;
 public class StringListSortTest {
     @Test
     public void testSelectionSort() {
-        
+        ArrayList<String> original;
+        ArrayList<String> expected;
+
+        original = new ArrayList<String> (Arrays.asList("apple","coconut", "banana", "dog"));
+        expected = new ArrayList<String> (Arrays.asList("apple","banana", "coconut", "dog"));
+        StringListSort.selectionSort(original);
+        assertEquals(expected, original);
+    }
+
+    @Test
+    public void testInsertionSort() {
+        ArrayList<String> original;
+        ArrayList<String> expected;
+
+        original = new ArrayList<String> (Arrays.asList("yogurt","dog", "banana", "zebra"));
+        expected = new ArrayList<String> (Arrays.asList("banana","dog", "yogurt", "zebra"));
+        StringListSort.insertionSort(original);
+        assertEquals(expected, original);
     }
 }
